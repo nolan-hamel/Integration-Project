25251fb979f0b634037cd7ba834c0c351205e034
nolan-hamel <nolanhamel@gmail.com>
2022-05-23 11:11:03 -0400

    initial commit

--- index.js
+++ index.js
@@ -44,9 +44,3 @@ app.get('/loads', (req, res) => {
 app.put('/messages/:handle', (req, res) => {
   
 })
-
-var server = app.listen(8081, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
-})
